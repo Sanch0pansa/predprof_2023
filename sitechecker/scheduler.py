5ec6a2a00b5fedc18d56b8e54da1a5eed7feafe9
kartofagen <74975693+kartofagen@users.noreply.github.com>
2023-01-14 22:18:21 +0300

    Create scheduler.py

---
+++ sitechecker/scheduler.py
@@ -0,0 +1,16 @@
+from site_checker import check
+import schedule
+import time
+
+
+def get_urls():
+    # тут будут вымогаться jsonы
+    urls = ['https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%B9%D1%82']
+    return urls
+
+
+def do():
+    return check(get_urls())
+
+
+schedule.every().hour.do(do)
