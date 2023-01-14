21b701aebab77a81b1dfa66fc64e6a20165ad353
kartofagen <74975693+kartofagen@users.noreply.github.com>
2023-01-14 21:45:45 +0300

    Update site_checker.py

--- sitechecker/site_checker.py
+++ sitechecker/site_checker.py
@@ -3,21 +3,54 @@ from time import time
 from datetime import datetime
 
 
+TIMEOUT_TIME = 7
+FIGOVO_GRUZIT_TIME = 5
+
+
 def opening_time(website):
     open_time = time()
     website.read()
     close_time = time()
     website.close()
-    return (close_time - open_time)
+    return close_time - open_time
 
 
+'''
+Всё хорошо - 200
+Плохо грузит - 199
+Timeout - 0
+Hostname mismatch - 1
+getaddrinfo failed - 2
+Непонятная фигня с URLError - 666
+
+Not Found - 404
+Method Not Allowed - 405
+Not Acceptable - 406
+и т.д. для HTTPError
+'''
 def check(urls):
     statuses = []
     for url in urls:
-        website = urllib.request.urlopen(url)
-        statuses.append((website.getcode() == 200 and opening_time(website) < 3,
-                         datetime.now()))
+        try:
+            website = urllib.request.urlopen(url, timeout=TIMEOUT_TIME)
+        except urllib.error.HTTPError as error:
+            status = error.code
+        except urllib.error.URLError as error:
+            if error.reason.args[0] == '_ssl.c:1106: The handshake operation timed out':
+                status = 0
+            elif error.reason.args[0] == 1:
+                status = 1
+            elif error.reason.args[0] == 11001:
+                status = 2
+            else:
+                status = 666
+        else:
+            if opening_time(website) < FIGOVO_GRUZIT_TIME:
+                status = website.getcode()
+            else:
+                status = 199
+        statuses.append((status, datetime.now()))
     return statuses
 
 
-print(check(['https://fedingo.com/python-script-to-check-url-status/']))
+print(check(['https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%B9%D1%82']))
