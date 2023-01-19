a57a92063f0fdbd1478f2a094027a653c0f6ac3c
Лебедев Александр <god@lifegame.su>
2023-01-19 22:56:48 +0300

    Реализована первая секция главной страницы

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,10 @@ import { RouterLink, RouterView } from 'vue-router'
 import PageHeader from "@/components/page/PageHeader.vue";
 import PageFooter from "@/components/page/PageFooter.vue";
 import PageTable from "@/components/page/PageTable.vue";
+import PageWelcomeSection from "@/components/page/PageWelcomeSection.vue";
 
 export default {
-  components: {PageHeader, PageFooter, PageTable},
+  components: {PageHeader, PageFooter, PageTable, PageWelcomeSection},
   data() {
     return {
       tableData: [
@@ -31,17 +32,7 @@ export default {
 <div class="wrapper">
   <PageHeader></PageHeader>
   <div class="container">
-    <h1 class="mt-5">Hello, vue + bootstrap</h1>
-    <h2 class="text-primary">Some text here</h2>
-<!--    <button class="btn btn-primary mt-3">Button</button>-->
-    <Btn :class="[ 'mt-3' ]">FGHFGH</Btn>
-    <div class="row mt-3">
-      <div class="col-7">
-        <Block>
-          <h3>Hello world</h3>
-        </Block>
-      </div>
-    </div>
+    <PageWelcomeSection></PageWelcomeSection>
     <div class="row">
       <div class="col-12">
         <PageSection :title="`Популярные ресурсы`">
