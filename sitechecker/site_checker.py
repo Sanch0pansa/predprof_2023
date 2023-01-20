10fc392f48177fb7d2032892dc81ecdb1eedba6f
kartofagen <74975693+kartofagen@users.noreply.github.com>
2023-01-20 22:21:02 +0300

    Вроде работает
    добавлено unknown url type и переделано для одной ссылки

--- sitechecker/site_checker.py
+++ sitechecker/site_checker.py
@@ -24,30 +24,28 @@ Not Found - 404
 Method Not Allowed - 405
 Not Acceptable - 406
 и т.д. для HTTPError
+
+unknown url type - -1
 '''
-def check(urls):
-    statuses = []
-    for url in urls:
-        try:
-            website = urllib.request.urlopen(url, timeout=TIMEOUT_TIME)
-        except urllib.error.HTTPError as error:
-            status = error.code
-        except urllib.error.URLError as error:
-            if error.reason.args[0] == '_ssl.c:1106: The handshake operation timed out':
-                status = 0
-            elif error.reason.args[0] == 1:
-                status = 1
-            elif error.reason.args[0] == 11001:
-                status = 2
-            else:
-                status = 666
+def check(url):
+    try:
+        website = urllib.request.urlopen(url, timeout=TIMEOUT_TIME)
+    except urllib.error.HTTPError as error:
+        status = error.code
+    except urllib.error.URLError as error:
+        if error.reason.args[0] == '_ssl.c:1106: The handshake operation timed out':
+            status = 0
+        elif error.reason.args[0] == 1:
+            status = 1
+        elif error.reason.args[0] == 11001:
+            status = 2
         else:
-            if opening_time(url) < FIGOVO_GRUZIT_TIME:
-                status = website.getcode()
-            else:
-                status = 199
-        statuses.append((status, datetime.now()))
-    return statuses
-
-
-print(check(['https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%B9%D1%82']))
+            status = 666
+    except ValueError:
+        status = -1
+    else:
+        if opening_time(url) < FIGOVO_GRUZIT_TIME:
+            status = website.getcode()
+        else:
+            status = 199
+    return status, datetime.now()
