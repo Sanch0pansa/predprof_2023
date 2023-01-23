aa35e2f5b535a58b233c12264cf0f27e5d07b10e
kartofagen <74975693+kartofagen@users.noreply.github.com>
2023-01-23 21:56:59 +0300

    Работа с сервером

--- sitechecker/scheduler.py
+++ sitechecker/scheduler.py
@@ -1,20 +1,23 @@
-from site_checker import check
 import schedule
 import time
 import requests
+from datetime import datetime
+
+from site_checker import check
 
 
-config = [i.split() for i in open('conf.txt').readlines()]
-checker_token = config[0][1]
+config = {}
+for l in open('conf.txt', 'r').readlines():
+    config[l.split(': ')[0]] = l.split(': ')[1]
+checker_token = config['checker_token']
 main_url = 'http://127.0.0.1:8000/api/v1/checker'
 
 
 def get_urls():
     data = requests.post(f'{main_url}/get_pages_for_check/', data={'_token': checker_token})
     urls_data = data.json()
-    urls = urls_data[0]['pages']
-
-    #urls = ['dstu.ru', 'donstu.ru']
+    urls = urls_data['pages']
+    #urls = ['donstu.ru', 'https://donstu.ru']
     return urls
 
 
@@ -26,15 +29,17 @@ def check_all_urls():
     print(results)
 
     # Отправка
-
+    for url, info in results.items():
+        response = requests.post(f'{main_url}/check/', data={'_token': checker_token, 'url': url, 'response_status_code': info[0], 'response_time': info[1]})
+        print(response.json())
 
     # Логи
     with open('logs.log', 'a') as logs:
         for url, info in results.items():
-            logs.write('\n' + str(info[0].strftime("%d-%b-%Y %H:%M:%S")) + ' >>> \t' + str(info[1]) + '\t| ' + str('%.3f' % info[2]) + '\t| ' + str(url))
+            logs.write('\n' + str(datetime.now().strftime("%d-%b-%Y %H:%M:%S")) + ' >>> \t' + str(info[0]) + '\t| ' + str('%.3f' % info[1]) + '\t| ' + str(url))
 
 
-schedule.every().second.do(check_all_urls)
+schedule.every().hour.do(check_all_urls)
 while True:
     schedule.run_pending()
     time.sleep(1)
