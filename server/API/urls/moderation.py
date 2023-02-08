a3b03bf66cbfaa01e68be4041bd0d274b8bfdfec
Ante <prizraksworld@gmail.com>
2023-02-08 17:59:56 +0300

    rejected reviews and reports

--- server/API/urls/moderation.py
+++ server/API/urls/moderation.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('moderation/pages/', moderation.GetModerationPages.as_view()),
     path('moderation/rejected_pages/', moderation.GetRejectedModerationPages.as_view()),
     path('moderation/reviews/', moderation.GetModerationReviews.as_view()),
+    path('moderation/rejected_reviews/', moderation.GetRejectedModerationReviews.as_view()),
     path('moderation/reports/', moderation.GetModerationReports.as_view()),
+    path('moderation/rejected_reports/', moderation.GetRejectedModerationReports.as_view()),
     path('moderation/moderate/<str:category>/<int:id>/', moderation.Moderate.as_view())
 ]
