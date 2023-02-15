8cd76f29d3ae034f3aee07c82022e8a1b63e1260
Лебедев Александр <god@lifegame.su>
2023-02-15 23:27:48 +0300

    Мелкие правки + глубокая проверка

---
+++ frontend/src/store/deepCheckModule.js
@@ -0,0 +1,104 @@
+import axios from 'axios'
+import Url from "@/store/url";
+
+
+const URLS = {
+    makeReport: level => `${Url}/page/make_report/${level}/`,
+}
+
+export default {
+    namespaced: true,
+    state: () => ({
+    }),
+    actions: {
+        // Получение первичных данных
+        async getFirstRequest({rootState}, {url}) {
+
+            try {
+                const resp = await axios.post(
+                    URLS.makeReport(1),
+                    {
+                        url: url,
+                    },
+                    {
+
+                    }
+                );
+
+                return resp.data;
+
+            } catch(e) {
+                return {
+                    id: 1, // ID для последующего дописывания
+
+                    has_data: false, // Есть ли инфа по этому ресурсу
+
+                    ping: 0.0065, // Пингование
+                    response_status_code: 200, // Код ответа
+                    response_time: 0.034, // Задержка ответа
+                };
+            }
+        },
+
+        // Получение данных Google Page Speed Insights
+        async getSecondRequest({rootState}, {id}) {
+
+            try {
+                const resp = await axios.post(
+                    URLS.makeReport(2),
+                    {
+                        id: id
+                    },
+                    {
+
+                    }
+                );
+
+                return resp.data;
+
+            } catch(e) {
+                return {
+                    first_contentful_paint: 0,
+                    first_meaningful_paint: 0,
+                    largest_contentful_paint: 0,
+                    speed_index: 0,
+                    full_page_loading_time: 0,
+                    score: 0,
+                };
+            }
+        },
+
+        // Получение данных по документу и, если есть, по проверкам и сообщениям об ошибках
+        async getThirdRequest({rootState}, {id, date_from, date_to}) {
+            try {
+                const resp = await axios.post(
+                    URLS.makeReport(3),
+                    {
+                        id: id,
+                        date_from: date_from,
+                        date_to: date_to
+                    },
+                    {
+
+                    }
+                );
+
+                return resp.data;
+
+            } catch(e) {
+                return {
+                    document_url: "https://google.com",
+                    other_check_reports: [
+                        {
+                            date: 'hfgh',
+                            document_url: "https://msu.ru"
+                        }
+                    ],
+                };
+            }
+        },
+    },
+
+    mutations: {
+    }
+}
