2d0af197f1a35b37b3242d02c78dd3edafde43cd
Лебедев Александр <god@lifegame.su>
2023-02-17 16:23:32 +0300

    Сворачивание событий

--- frontend/src/components/items/EventsList.vue
+++ frontend/src/components/items/EventsList.vue
@@ -1,6 +1,11 @@
 <template>
   <ul class="list-group list-group-flush border-top">
-    <EventsListItem :data="event" v-for="event in events"></EventsListItem>
+    <EventsListItem :data="event" v-for="event in getEvents()"></EventsListItem>
+
+    <li class="list-group-item card-body py-3" v-if="events.length > 3">
+      <span class="text-primary" role="button" @click="() => {opened = !opened}">{{ opened ? "Свернуть" : `Развернуть (eщё ${events.length - 3})` }}</span>
+    </li>
+    <li class="list-group-item card-body py-3" v-if="events.length === 0"><span class="text-muted">Нет событий</span></li>
   </ul>
 </template>
 
@@ -14,6 +19,20 @@ export default {
     events: {
       required: true,
       type: Array,
+    }
+  },
+  data() {
+    return {
+      opened: false,
+    }
+  },
+  methods: {
+    getEvents() {
+      if (this.opened) {
+        return this.events;
+      } else {
+        return this.events.slice(0, 3);
+      }
     }
   }
 }
